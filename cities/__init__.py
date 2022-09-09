01968e1b4667018e45afbb2d0bca0c93089e36b3
pekshevds <pekshev.ds@gmail.com>
2022-09-09 13:12:30 +0300

    implementation game logic

---
+++ cities/__init__.py
@@ -0,0 +1,32 @@
+import pathlib 
+import random
+
+def _read_cities_from_file()->list[str]:
+    """
+    read file with cities
+    """
+
+    file_name = pathlib.Path(pathlib.Path.cwd(), 'cities','cities.txt')
+    with open(file_name, mode='r', encoding='utf-8') as file:
+        cities = set(line.strip() for line in file.readlines() if line.strip() != "")
+    
+    return list(cities)
+
+
+def find_cities(city:str, cities:list)->list[str]:
+    """
+    finds cities by last city character
+    """
+    
+    return [_ for _ in cities if _.lower().startswith(city.lower()[-1])]
+
+
+def get_random_city(cities:list)->str:
+    """
+    get random city from city list
+    """
+
+    return random.choice(cities)
+
+
+CITIES = _read_cities_from_file()
---
+++ game.py
@@ -0,0 +1,14 @@
+import cities
+from parser import init_parser
+
+if __name__ == "__main__":
+
+    parser = init_parser() 
+    args = parser.parse_args()
+
+    if  args.city:
+
+        found_cities = cities.find_cities(args.city, cities=cities.CITIES)
+        random_city = cities.get_random_city(found_cities)
+        
+        print(f"--found cities:{found_cities}\n--total found cities:{len(found_cities)}\n--random city:{random_city}")
