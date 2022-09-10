1e7cf23d79ba51b5ea243e5bc520246bea03fae7
pekshevds <pekshev.ds@gmail.com>
2022-09-10 09:46:14 +0300

    corrected mistakes

--- test.py
+++ test.py
@@ -4,14 +4,19 @@ from cities import *
 def test_find_cities():
     
     for counter in range(3):
+        
         random_city = get_random_city(CITIES)
         original = sorted(find_cities(city=random_city, cities=CITIES))        
+        
         assert sorted(find_cities(random_city, cities=CITIES)) == original
+
 
 def test_get_random_city():
         
     for counter in range(3):
+        
         random_city = get_random_city(CITIES)
         original = sorted(find_cities(city=random_city, cities=CITIES))
+        
         assert get_random_city(original) in original
     
